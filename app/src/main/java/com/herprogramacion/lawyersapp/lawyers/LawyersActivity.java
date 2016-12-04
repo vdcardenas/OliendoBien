eb058d0f1e4c36637b9e8d6411db0641e43aeec6
neuhaus <victordecardenas@gmail.com>
2016-12-04 21:50:32 +0100

    Initial commit

---
+++ app/src/main/java/com/herprogramacion/lawyersapp/lawyers/LawyersActivity.java
@@ -0,0 +1,31 @@
+package com.herprogramacion.lawyersapp.lawyers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.herprogramacion.lawyersapp.R;
+
+public class LawyersActivity extends AppCompatActivity {
+
+    public static final String EXTRA_LAWYER_ID = "extra_lawyer_id";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lawyers);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        LawyersFragment fragment = (LawyersFragment)
+                getSupportFragmentManager().findFragmentById(R.id.lawyers_container);
+
+        if (fragment == null) {
+            fragment = LawyersFragment.newInstance();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.lawyers_container, fragment)
+                    .commit();
+        }
+    }
+}
